6ca448208f48f8e1e76b5e0c745b79b967cc66de
Jopau9 <jprahfer@gmail.com>
2025-12-16 21:07:00 +0100

    foto

--- app/components/AboutMe/AboutMe.tsx
+++ app/components/AboutMe/AboutMe.tsx
@@ -11,33 +11,51 @@ export default function AboutMe() {
     <section
       className="
         w-full
-        py-16                 /* separació vertical normal */
-        flex flex-col
-        gap-12                /* espai entre blocs, no exagerat */
+        py-16
+        grid grid-cols-1 lg:grid-cols-2
+        gap-16
+        justify-items-center
+        
       "
     >
-      {/* Títol */}
-      <AboutMeTitle />
+      {/* COLUMNA ESQUERRA */}
+      <div className="flex flex-col gap-12 pl-0">
+        <AboutMeTitle />
 
-      {/* Text + CV + text lateral */}
-      <div className="mt-4 ">
-        <AboutMeText1 />
+        <div className="pl-20">
+          <AboutMeText1 />
 
-        <div
-          className="
-            mt-8 
-            flex flex-col lg:flex-row    /* en columna al mòbil, en fila al desktop */
-            items-start lg:items-center 
-            ml-40
-          "
-        >
-          <DownloadCV />
-          <AboutMeText2 />
+          <div className="mt-8 flex flex-col lg:flex-row items-start lg:items-center gap-6 pl-40">
+            <DownloadCV />
+            <AboutMeText2 />
+          </div>
         </div>
       </div>
 
-      {/* Frase / quote centrada */}
-      <div className="mt-25 mb-6 flex justify-center">
+      {/* COLUMNA DRETA */}
+      <div className="lg:flex justify-center items-start">
+  <img
+    src="/personal_photos/Adobe Express - file.png"
+    alt="Portrait"
+    className="w-full max-w-md rounded-2xl object-cover"
+    style={{
+      WebkitMaskImage: `
+        linear-gradient(to left, transparent 0%, black 200px),
+        linear-gradient(to top, transparent 0%, black 500px)
+      `,
+      WebkitMaskComposite: 'intersect',
+      maskImage: `
+        linear-gradient(to left, transparent 5%, black 200px),
+        linear-gradient(to top, transparent 0%, black 200px)
+      `,
+      maskComposite: 'intersect'
+    }}
+  />
+</div>
+
+
+      {/* QUOTE A SOTA, CENTRADA */}
+      <div className="lg:col-span-2 mt-8 flex justify-center">
         <QuoteBlock />
       </div>
     </section>
